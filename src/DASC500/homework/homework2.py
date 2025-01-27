5b597ec647445d64db9b652a8bdefccdcfec7d53
mscotty <mitchell.scott.394@gmail.com>
2025-01-26 23:04:52 -0500

    last minute correction to improve figure

--- src/DASC500/homework/homework2.py
+++ src/DASC500/homework/homework2.py
@@ -16,7 +16,7 @@ def plotting_csv():
     data_obj.plot_clustered_bar_chart_horizontal(x_column=None, y_column='City', output_dir=output_folder, x_axis_name='Percentage of Time (%)')
     data_obj.plot_individual_bar_charts(x_column='City', y_column=None, output_dir=output_folder, y_axis_name='Percentage of Time(%)')
     data_obj.plot_line_chart(x_column='City', y_column='Idle', output_dir=output_folder)
-    data_obj.plot_heatmap(output_dir=output_folder, title_name='Heatmap of Values', x_axis_name='Categories', y_axis_name='Cities')
+    data_obj.plot_heatmap(output_dir=output_folder, title_name='Heatmap of % Time Utilization', x_axis_name='Categories', y_axis_name='Cities')
     data_obj.plot_radar_chart(output_dir=output_folder, title_name='Radar Chart of Categories by City')
 
 
